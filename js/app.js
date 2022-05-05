a19e40dd6a47f021dba60bd6fca74141de863b96
nicolas leone <leoneklyver@gmail.com>
2022-05-05 20:21:39 -0300

    guardando challenge14

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Obtener un número del usuario y mostrar en consola si es par o impar
+
+let number = prompt('Dame un numero');
+
+number %2==0? console.log('su numero es par') : console.log('su numero es impar');
